4d45c13ba05a713f8b824e6b0b053b9c252f0781
skalermo <romnmko@gmail.com>
2019-07-25 22:05:18 +0300

    Assigned actions for open and close MenuItems (with hotkeys)

--- src/view/MainWindowController.java
+++ src/view/MainWindowController.java
@@ -1,11 +1,12 @@
 package view;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -16,15 +17,16 @@ public class MainWindowController implements Initializable {
     // These are for Menu Items
     @FXML private MenuBar menuBar;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    // These are for ScrollPane and InfoLabels
+    @FXML private ScrollPane scrollPane;
+    @FXML private Label newFileLabel;
+    @FXML private Label openFileLabel;
 
-    }
 
     /**
      * This method closes the stage
      */
-    public void closeMenuItemAction()
+    public void quitMenuItemAction()
     {
         // This line gets the Stage information
         Stage window = (Stage)menuBar.getScene().getWindow();
@@ -32,4 +34,40 @@ public class MainWindowController implements Initializable {
         window.close();
     }
 
+
+    /**
+     * This method enables and shows ScrollPane which contain TableView
+     * also hides InfoLabels
+     */
+    public void newMenuItemAction()
+    {
+        if (!scrollPane.isDisabled())
+            return;
+        scrollPane.setDisable(false);
+        scrollPane.setOpacity(1.0);
+        newFileLabel.setDisable(true);
+        newFileLabel.setOpacity(0.0);
+        openFileLabel.setDisable(true);
+        openFileLabel.setOpacity(0.0);
+    }
+
+    /**
+     * This method disables and hides ScrollPane
+     * and shows InfoLabels
+     */
+    public void closeMenuItemAction()
+    {
+        if (scrollPane.isDisabled())
+            return;
+        scrollPane.setDisable(true);
+        scrollPane.setOpacity(0.0);
+        newFileLabel.setDisable(false);
+        newFileLabel.setOpacity(1.0);
+        openFileLabel.setDisable(false);
+        openFileLabel.setOpacity(1.0);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
