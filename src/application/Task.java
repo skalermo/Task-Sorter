f7a7f182601579d36ba0e744d5b5c4617d415692
skalermo <romnmko@gmail.com>
2019-07-25 22:26:03 +0300

    Added Task class

---
+++ src/application/Task.java
@@ -0,0 +1,61 @@
+package application;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Task implements Serializable {
+    private SimpleStringProperty taskName;
+    private SimpleIntegerProperty taskComplexity;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Task(String taskName, int taskComplexity, LocalDate startDate, LocalDate endDate) {
+        this.taskName = new SimpleStringProperty(taskName);
+        this.taskComplexity = new SimpleIntegerProperty(taskComplexity);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getTaskName() {
+        return taskName.get();
+    }
+
+    public SimpleStringProperty taskNameProperty() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName.set(taskName);
+    }
+
+    public int getTaskComplexity() {
+        return taskComplexity.get();
+    }
+
+    public SimpleIntegerProperty taskComplexityProperty() {
+        return taskComplexity;
+    }
+
+    public void setTaskComplexity(int taskComplexity) {
+        this.taskComplexity.set(taskComplexity);
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
